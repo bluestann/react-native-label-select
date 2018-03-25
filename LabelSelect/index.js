42e02573d0c834f20ee71f2ac44cb1817c354609
bluestann <bluestann@gmail.com>
2018-03-25 16:10:22 -0700

    PropTypes since React v15.5

--- LabelSelect/index.js
+++ LabelSelect/index.js
@@ -2,7 +2,8 @@
  * Created by TinySymphony on 2017-01-03.
  */
 
-import React, {Component, PropTypes} from 'react';
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
 import {
   View,
   Text,
